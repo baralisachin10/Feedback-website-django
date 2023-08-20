499621f78184a9f32a17179b3f91605b8b7540ac
sacheen <baralisachin@gmail.com>
2023-08-20 23:02:00 +0545

    class based views

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic.base import View
 
 # Create your views here.
 
-def profile(request):
-    return render(request, 'profiles/create_profile.html')
+class CreateProfileView(View):
+    def get(self,request):
+        return render(request,"profiles/create_profile.html")
+    
+    def post(self,request):
+        print(request.FILES["image"])
+        return redirect("/profile")

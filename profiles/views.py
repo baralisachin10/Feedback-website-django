eef040cd0a40fcecb93b1e0f70e88907ad1b44a8
sacheen <baralisachin@gmail.com>
2023-08-21 09:34:05 +0545

    file uploads

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
+from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
 class CreateProfileView(View):
-    def get(self,request):
-        return render(request,"profiles/create_profile.html")
-    
-    def post(self,request):
-        print(request.FILES["image"])
-        return redirect("/profile")
+    def get(self, request):
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {"form": form})
+
+    def post(self, request):
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            profile = UserProfile(image = request.FILES["users_image"])
+            profile.save()
+            return redirect("/profile")
+
+        return render(request, "profiles/create_profile.html", {"form": submitted_form})
